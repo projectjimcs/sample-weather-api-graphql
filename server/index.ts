3c1f1166c2e92d2fcf0843dd46e574e46564505e
Jim <projectjimcs@gmail.com>
2022-07-19 20:12:31 -0700

    Graphql types added

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,34 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { graphqlHTTP } from 'express-graphql';
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
+import { RootQueryType} from './qlmodules/types';
 
 dotenv.config();
 
+// const schema: GraphQLSchema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: 'HelloWorld',
+//     fields: () => ({
+//       message: {
+//         type: GraphQLString,
+//         resolve: () => 'Hello World',
+//       }
+//     })
+//   })
+// })
+
 const app: Express = express();
 const port = process.env.PORT;
+
+const schema: GraphQLSchema = new GraphQLSchema({
+  query: RootQueryType
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
